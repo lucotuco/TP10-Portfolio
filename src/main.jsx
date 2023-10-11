15d294ebb28dc83affd312a35fb053d2e6c3e35b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-11 09:01:38 -0300

    navbar:)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './Layout'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <BasicExample/>
   </React.StrictMode>,
 )
